ff0873628f1a938ecf447b136553c83164b306ff
zarik <fff@dd.dd>
2015-05-08 22:55:29 +0300

    auto

--- fbr/urls.py
+++ fbr/urls.py
@@ -8,13 +8,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
      url(r'^$', 'map.views.home', name='home'),
-     url(r'^cot$', 'map.views.cot', name='cot'),  
+     url(r'^cotter$', 'map.views.cot', name='cotter'),  
      url(r'^radiation$', 'map.views.radiation', name='radiation'),  
      url(r'^slope$', 'map.views.slope', name='slope'),  
     #url(r'^test$', 'map.views.test', name='test'),  
 
 
-    url(r'^cotter/$', GetPolygonJsonCotter.as_view(model=Cotter,properties=('gid','veg_types')), name='cotter'),
+    url(r'^cotter-json/$', GetPolygonJsonCotter.as_view(model=Cotter,properties=('gid','veg_types'))),
 
     url(r'^admin/', include(admin.site.urls)),
 )
