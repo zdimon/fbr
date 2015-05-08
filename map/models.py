69cfc38de541921efa8f0648e9b82b64696933be
zarik <fff@dd.dd>
2015-05-08 23:32:15 +0300

    auto

--- map/models.py
+++ map/models.py
@@ -16,8 +16,8 @@ class Cotter(models.Model):
 
 class Slope(models.Model):
     gid = models.IntegerField(_(u'Primary key'), db_index=True, primary_key=True)
-    veg_key = models.IntegerField(_(u'Veg key'))
-    veg_types = models.IntegerField(_(u'Veg type'))
+    id = models.IntegerField(_(u'Id'))
+    gridcode = models.IntegerField(_(u'Grid code'))
     geom = models.MultiPolygonField(null=True, blank=True)
     def __unicode__(self):
         return 'Slope #%s' % self.gid
