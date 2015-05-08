af00667a4d70a7eaec627d276fdaa1cabe45e1a1
zdimon <zdimon77@gmail.com>
2015-05-08 19:28:47 +0300

    auto

--- map/admin.py
+++ map/admin.py
@@ -12,7 +12,7 @@ class MapLayer(GeoJSONLayerView):
     simplify = 0.5  # generalization
 
 
-class CotterAdmin(admin.ModelAdmin):
+class CotterAdmin(OSMGeoAdmin):
     map_template = 'gis/admin/google.html'
     extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
 
@@ -20,14 +20,14 @@ admin.site.register(Cotter, CotterAdmin)
 
 
 
-class SlopeAdmin(admin.ModelAdmin):
+class SlopeAdmin(OSMGeoAdmin):
     map_template = 'gis/admin/google.html'
     extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
 
 admin.site.register(Slope, SlopeAdmin)
 
 
-class RadiationAdmin(admin.ModelAdmin):
+class RadiationAdmin(OSMGeoAdmin):
     map_template = 'gis/admin/google.html'
     extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
 
