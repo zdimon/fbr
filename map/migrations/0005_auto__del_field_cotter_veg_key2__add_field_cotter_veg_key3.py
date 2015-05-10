2f36f68902b4115be7d4dfe2395c52bccf1b6738
Your Name <zdimon77@gmail.com>
2015-05-10 16:35:10 +0300

    from server

---
+++ map/migrations/0005_auto__del_field_cotter_veg_key2__add_field_cotter_veg_key3.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Cotter.veg_key2'
+        db.delete_column(u'map_cotter', 'veg_key2')
+
+        # Adding field 'Cotter.veg_key3'
+        db.add_column(u'map_cotter', 'veg_key3',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Adding field 'Cotter.veg_key2'
+        db.add_column(u'map_cotter', 'veg_key2',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+        # Deleting field 'Cotter.veg_key3'
+        db.delete_column(u'map_cotter', 'veg_key3')
+
+
+    models = {
+        u'map.cotter': {
+            'Meta': {'object_name': 'Cotter'},
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'veg_key': ('django.db.models.fields.IntegerField', [], {}),
+            'veg_key3': ('django.db.models.fields.IntegerField', [], {}),
+            'veg_types': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'map.radiation': {
+            'Meta': {'object_name': 'Radiation'},
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'gridcode': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'map.slope': {
+            'Meta': {'object_name': 'Slope'},
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'gridcode': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['map']
