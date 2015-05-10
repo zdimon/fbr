67064e71ca7a4f9baa762b68f8649370de684e53
zdimon <zdimon77@gmail.com>
2015-05-10 11:32:05 +0300

    auto

--- map/models.py
+++ map/models.py
@@ -34,7 +34,7 @@ class Radiation(models.Model):
     geom = models.MultiPolygonField(null=True, blank=True)
     def __unicode__(self):
 #        return 'Radiation #%s' % self.gridcode
-        return u'%s %s' % (self.id, self.gridcode)
+        return u'id - %s gridcode - %s' % (self.id, self.gridcode)
 
     class Meta:
         verbose_name=_(u'Radiation')
