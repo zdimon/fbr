b0b7a0d1f4673f0bb721cc68b004bf71bf41e0eb
zarik <fff@dd.dd>
2015-05-10 15:55:41 +0300

    auto

--- fabfile.py
+++ fabfile.py
@@ -33,9 +33,9 @@ def deploy():
         #run('bower install')
         run('./manage.py schemamigration map --auto') # Собираем статику
         run('./manage.py migrate')
-        run('git add --all')
-        run('git commit -m "from server"')
-        run('git push')
+        #run('git add --all')
+        #run('git commit -m "from server"')
+        #run('git push')
         #run('find . -name "*.mo" -print -delete')  # Чистим старые скомпиленные файлы gettext'а
         #run('./manage.py compilemessages')  # Собираем новые файлы gettext'а
         #run('sudo supervisorctl restart all')
