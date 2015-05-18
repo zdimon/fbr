b231ffbe94935501f684a13974cff43dce9ced63
zdimon <zdimon77@gmail.com>
2015-05-18 10:26:12 +0300

    auto

--- map/admin.py
+++ map/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from map.models import Cotter, Slope, Radiation, Vegetation, Structure, Veget
+from map.models import Cotter, Slope, Radiation, Vegetation, Structure, Veget, Direction
 from djgeojson.views import GeoJSONLayerView
 from django.contrib.gis.admin import OSMGeoAdmin, GeoModelAdmin
 from django.conf import settings
@@ -38,6 +38,19 @@ class VegetationAdmin(OSMGeoAdmin):
     extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
 
 admin.site.register(Vegetation, VegetationAdmin)
+
+
+class DirectionAdmin(OSMGeoAdmin):
+    list_display = ("gid", "direction")
+    search_fields = ("gid", "direction")
+   # list_filter = ("struct",)  
+    ordering = ('gid',) 
+    fields = ('gid', 'direction') 
+    map_template = 'gis/admin/google.html'
+    extra_js = ['http://openstreetmap.org/openlayers/OpenStreetMap.js', 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s' % settings.GOOGLE_MAPS_API_KEY]
+
+admin.site.register(Direction, DirectionAdmin)
+
 
 
 class VegetAdmin(OSMGeoAdmin):
