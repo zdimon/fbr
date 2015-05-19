dffbc09de4d0d8bb45178f076d9936d3673ec48e
zdimon <zdimon77@gmail.com>
2015-05-19 10:06:24 +0300

    auto

--- map/models.py
+++ map/models.py
@@ -9,12 +9,13 @@ class Cotter(models.Model):
     veg_key = models.IntegerField(_(u'Veg key'))
     veg_types = models.IntegerField(_(u'Veg key'))
     geom = models.MultiPolygonField(null=True, blank=True)
+    objects = models.GeoManager()
     def __unicode__(self):
         return 'Vegetation_ #%s  (%s)' % (self.gid,self.veg_types)
 
     class Meta:
         verbose_name=_(u'Vegetation')
-        verbose_name_plural=_(u'Vegetation')
+        verbose_name_plural=_(u'Vegeobjects = models.GeoManager()tation')
 
 
 class Structure(models.Model):
@@ -27,7 +28,7 @@ class Structure(models.Model):
   #      return '%s  (%s)' % (self.structure,self.fuel_moisture)
         return '%s' % (self.structure)
     @property
-    def color_repr(self):
+    def color_repr(self):objects = models.GeoManager()
         return mark_safe('<div style="width: 50px; height: 50px; background-color: %s"></div>' % self.color)
     class Meta:
         verbose_name=_(u'Structure')
@@ -43,6 +44,7 @@ class Veget(models.Model):
     struct = models.ForeignKey(Structure, null=True, blank=True)
 
     geom = models.MultiPolygonField(null=True, blank=True)
+    objects = models.GeoManager()
     def __unicode__(self):
         return 'Vegetation_ #%s  (%s)' % (self.gid,self.struct)
 
@@ -58,7 +60,7 @@ class Vegetation(models.Model):
     objectid = models.IntegerField(_(u'Objectid'))
     area = models.DecimalField(_(u'Area'), max_digits=15, decimal_places=3, null=True, blank=True)
     perimeter = models.DecimalField(_(u'Perimeter'), max_digits=25, decimal_places=15, null=True, blank=True)
-    veg = models.DecimalField(_(u'Veg'), max_digits=10, decimal_places=5, null=True, blank=True)
+    veg = models.DecimalField(_(u'Veg'), max_digits=10, decimal_places=5, null=True, blank=True)objects = models.GeoManager()
     veg_id = models.DecimalField(_(u'Veg_id'), max_digits=10, decimal_places=5, null=True, blank=True)
     veg_sp_1 = models.CharField(_(u'Veg_sp_1'), max_length=25, null=True, blank=True)
     veg_sp_2 = models.CharField(_(u'Veg_sp_2'), max_length=25, null=True, blank=True)
@@ -76,6 +78,7 @@ class Vegetation(models.Model):
     acres = models.DecimalField(_(u'Acres'), max_digits=20, decimal_places=15, null=True, blank=True)
     hectares = models.DecimalField(_(u'Hectares'), max_digits=25, decimal_places=15, null=True, blank=True)      
     geom = models.MultiPolygonField(null=True, blank=True)
+    objects = models.GeoManager()
     def __unicode__(self):
         return 'Vegetation_ #%s  (%s)' % (self.gid,self.veg_sp_1)
 
@@ -93,6 +96,7 @@ class Slope(models.Model):
     id = models.IntegerField(_(u'Id'))
     gridcode = models.IntegerField(_(u'Grid code'))
     geom = models.MultiPolygonField(null=True, blank=True)
+    objects = models.GeoManager()
     def __unicode__(self):
     #    return 'Slope #%s' % self.gridcode
         return u'id - %s gridcode - %s' % (self.id, self.gridcode)
@@ -107,6 +111,7 @@ class Radiation(models.Model):
     id = models.IntegerField(_(u'Id'))
     gridcode = models.IntegerField(_(u'Grid code'))
     geom = models.MultiPolygonField(null=True, blank=True)
+    objects = models.GeoManager()
     def __unicode__(self):
 #        return 'Radiation #%s' % self.gridcode
         return u'id - %s gridcode - %s' % (self.id, self.gridcode)
