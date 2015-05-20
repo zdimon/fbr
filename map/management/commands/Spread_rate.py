fc2416f5018c051686db44179f40279361494d6f
zdimon <zdimon77@gmail.com>
2015-05-20 10:25:11 +0300

    auto

--- map/management/commands/Spread_rate.py
+++ map/management/commands/Spread_rate.py
@@ -11,7 +11,7 @@ from decimal import Decimal
 logger = logging.getLogger(__name__)
 
 
-class Command(BaseCommand):
+class Command(BaseCommand):on level to undulating ground
     option_list = BaseCommand.option_list + (
         make_option("-t", "--temperature",
                     dest="temperature",
@@ -24,16 +24,20 @@ class Command(BaseCommand):
                     help=u"Drought index"),
         make_option("-n", "--rain",
                     dest="rain",
-                    help=u"Time rain"),  
+                    help=u"Time rain"),  on level to undulating ground
         make_option("-p", "--precipitation",
-                    dest="precipitation",
+                    dest="precipitation",   
                     help=u"Precipittion"),
         make_option("-w", "--wind",
                     dest="wind",
                     help=u"Wind speed"), 
         make_option("-l", "--load",
                     dest="load",
-                    help=u"Fuel Load"),                                          
+                    help=u"Fuel Load"),  
+        make_option("-s", "--slope",
+                    dest="slope",
+                    help=u"Slope on ground surface"),
+                                                                                                     
                                                                        
     )
     def handle(self, *args, **options):
@@ -46,14 +50,18 @@ class Command(BaseCommand):
         p = options["precipitation"]
         w = options["wind"]
         l = options["load"]
+        s = options["slope"]
         # Drought factor
         d = ((Decimal('0.191')) * (Decimal(i) + Decimal('104')) * (Decimal(n) + Decimal('1'))**(Decimal('1.5'))) / (Decimal('3.5')*(Decimal(n) + Decimal('1'))**(Decimal('1.5')) + Decimal(p) - Decimal('1'))
         #Fire danger index
-        f = Decimal('1.25') * Decimal (d) * ((Decimal(t) - Decimal(u)) / Decimal('30') + Decimal('0.0234') * Decimal(w))
-        #Rate of forward spread of fire
+        f = Decimal('1.25') * Decimal (d) * ((Decimal(t) - Decimal(u)) / Decimal('30') + Decimal('0.0234') * Decimal(w)) 
+        #Rate of forward spread of fire on level to undulating ground
         r = Decimal('0.0012') * Decimal(f) * Decimal(l)
+        #Rate of spread on ground of slope 
+        rs = Decimal(r) * (Decimal('0.069') * Decimal(s))
  
-        
+         
         print d
         print f
         print r
+        print rs
