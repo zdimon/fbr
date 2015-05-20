4cd0bac8c40bf7d2a4a274764ddaa69c7b65a25c
zdimon <zdimon77@gmail.com>
2015-05-20 08:49:54 +0300

    auto

--- map/management/commands/Spread_rate.py
+++ map/management/commands/Spread_rate.py
@@ -39,7 +39,7 @@ class Command(BaseCommand):
         n = options["rain"]
         p = options["precipitation"]
         
-        d = (Decimal('0.191'))
+        d = (Decimal('0.191')) * (Decimal(i) + Decimal('104'))
       #  d = (Decimal('0.191') * (Decimal(i) + Decimal('104')) * (Decimal(n) + Decimal('1'))**(Decimal('1.5'))) / (Decimal('3.5') * (Decimal(n) + Decimal(1'))**(Decimal('1.5')) + Decimal(P) - Decimal('1'))
         print "!!!!!!!!!!!!!!!!!!!!!!!!!"
         print d
