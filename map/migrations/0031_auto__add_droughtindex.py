e0be1f57c9eb2202c476aea2c626be5a2ce7ab7d
Your Name <zdimon77@gmail.com>
2015-05-21 10:02:48 +0300

    from server

---
+++ map/migrations/0031_auto__add_droughtindex.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'DroughtIndex'
+        db.create_table(u'map_droughtindex', (
+            ('gid', self.gf('django.db.models.fields.IntegerField')(primary_key=True, db_index=True)),
+            ('month', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['map.Month'], null=True, blank=True)),
+            ('direction', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['map.Direction'], null=True, blank=True)),
+            ('value', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=2, decimal_places=1, blank=True)),
+        ))
+        db.send_create_signal(u'map', ['DroughtIndex'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'DroughtIndex'
+        db.delete_table(u'map_droughtindex')
+
+
+    models = {
+        u'map.cotter': {
+            'Meta': {'object_name': 'Cotter'},
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'veg_key': ('django.db.models.fields.IntegerField', [], {}),
+            'veg_types': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'map.direction': {
+            'Meta': {'object_name': 'Direction'},
+            'direction': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'})
+        },
+        u'map.droughtindex': {
+            'Meta': {'object_name': 'DroughtIndex'},
+            'direction': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['map.Direction']", 'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'month': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['map.Month']", 'null': 'True', 'blank': 'True'}),
+            'value': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '2', 'decimal_places': '1', 'blank': 'True'})
+        },
+        u'map.effectiveness': {
+            'Meta': {'object_name': 'Effectiveness'},
+            'color': (u'colorful.fields.RGBColorField', [], {}),
+            'effectiveness': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'})
+        },
+        u'map.month': {
+            'Meta': {'object_name': 'Month'},
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'month': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'})
+        },
+        u'map.radiation': {
+            'Meta': {'object_name': 'Radiation'},
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'gridcode': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'map.slope': {
+            'Meta': {'object_name': 'Slope'},
+            'effectiv': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['map.Effectiveness']", 'null': 'True', 'blank': 'True'}),
+            'effectiveness': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '3', 'blank': 'True'}),
+            'effectiveness_category': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'gridcode': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.IntegerField', [], {}),
+            'litter': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            'structure': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'})
+        },
+        u'map.structure': {
+            'Meta': {'object_name': 'Structure'},
+            'color': (u'colorful.fields.RGBColorField', [], {}),
+            'fuel_load': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            'fuel_moisture': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'litter': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '2', 'blank': 'True'}),
+            'structure': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '25', 'null': 'True', 'blank': 'True'})
+        },
+        u'map.veget': {
+            'Meta': {'object_name': 'Veget'},
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'gridcode': ('django.db.models.fields.IntegerField', [], {}),
+            'struct': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['map.Structure']", 'null': 'True', 'blank': 'True'}),
+            'structure': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'})
+        },
+        u'map.vegetation': {
+            'Meta': {'object_name': 'Vegetation'},
+            'acres': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '20', 'decimal_places': '15', 'blank': 'True'}),
+            'area': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '15', 'decimal_places': '3', 'blank': 'True'}),
+            'class1': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'fuelcode_v': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'fueltype': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'geom': ('django.contrib.gis.db.models.fields.MultiPolygonField', [], {'null': 'True', 'blank': 'True'}),
+            'gid': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True', 'db_index': 'True'}),
+            'hectares': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '25', 'decimal_places': '15', 'blank': 'True'}),
+            'objectid': ('django.db.models.fields.IntegerField', [], {}),
+            'perimeter': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '25', 'decimal_places': '15', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '6', 'null': 'True', 'blank': 'True'}),
+            'struct': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['map.Structure']", 'null': 'True', 'blank': 'True'}),
+            'structure': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            'symbol': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '20', 'decimal_places': '15', 'blank': 'True'}),
+            'veg': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '5', 'blank': 'True'}),
+            'veg_id': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '5', 'blank': 'True'}),
+            'veg_sp_1': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            'veg_sp_2': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'}),
+            'veg_sp_3': ('django.db.models.fields.CharField', [], {'max_length': '25', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['map']
