3794aa33d8649c041640f421ace253a9922154c2
zarik <fff@dd.dd>
2015-05-22 20:35:49 +0300

    auto

--- map/firesim.py
+++ map/firesim.py
@@ -30,9 +30,10 @@ def fire_count(request,time):
     except:
         s = Burning.objects.get(burning=1)
         dist = Decimal(0.001)*Decimal(time)
-        for r in Burning.objects.filter(geom__dwithin=(s.geom , dist)):
-            r.burning = 1
-            r.save()            
+        Burning.objects.filter(geom__dwithin=(s.geom , dist)).update(burning=1)
+        #for r in Burning.objects.filter(geom__dwithin=(s.geom , dist)):
+        #    r.burning = 1
+        #    r.save()            
     
     html = time
     return HttpResponse(html)
