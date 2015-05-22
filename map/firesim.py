aefb638ac02765005502d4d267f17bf7eefaf655
zarik <fff@dd.dd>
2015-05-22 18:13:22 +0300

    auto

--- map/firesim.py
+++ map/firesim.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 
-def fire_init(request):
+def fire_init(request,gid):
+    Burning.objects.all().delete()
+    r = Radiation.objects.get(pk=gid)
+    b = Burning()
+    b.burning = 1
+    b.day = 1
+    b.time = 1
+    b.geom = r.geom
+    b.gridcode = r.gridcode
+    b.save()
     html = 'Time:<input name="time">'
     return HttpResponse(html)
 
