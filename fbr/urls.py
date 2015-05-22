31cdbaab99a60b6cdf53d2aa61ef2a84d56c55bf
zdimon <zdimon77@gmail.com>
2015-05-22 10:32:34 +0300

    auto

--- fbr/urls.py
+++ fbr/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-from map.views import GetPolygonJsonCotter, GetPolygonJsonRadiation, GetPolygonJsonVegetation, GetPolygonJsonVeget, GetPolygonJsonSlope, GetPolygonJsonVegetType, GetPolygonJsonNdvi001250
-from map.models import Cotter, Radiation, Vegetation, Veget, Slope, Effectiveness, Ndvi001250
+from map.views import GetPolygonJsonCotter, GetPolygonJsonRadiation, GetPolygonJsonVegetation, GetPolygonJsonVeget, GetPolygonJsonSlope, GetPolygonJsonVegetType, GetPolygonJsonNdvi001250, GetPolygonBurning
+from map.models import Cotter, Radiation, Vegetation, Veget, Slope, Effectiveness, Ndvi001250, Burning
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,12 +10,14 @@ urlpatterns = patterns('',
      url(r'^$', 'map.views.radiation', name='home'),
      url(r'^cotter/$', 'map.views.cot', name='cotter'),
      url(r'^vegetation/$', 'map.views.vegetation', name='vegetation'),
+     url(r'^burning/$', 'map.views.burning', name='burning'),
      url(r'^vegetstructure/$', 'map.views.veget', name='vegetation structure'),  
      url(r'^radiation$', 'map.views.radiation', name='radiation'),  
      url(r'^slope$', 'map.views.slope', name='slope'),  
      url(r'^radiation_json$', 'map.views.radiation_json', name='radiation_json'),
      url(r'^ndvi001250_json$', 'map.views.ndvi001250_json', name='ndvi001250_json'),
-     url(r'^vegetation_json$', 'map.views.vegetation_json', name='vegetation_json'),  
+     url(r'^vegetation_json$', 'map.views.vegetation_json', name='vegetation_json'),
+     url(r'^burning_json$', 'map.views.burning_json', name='burning_json'),  
      url(r'^crap$', 'map.views.home', name='crap'),  
      url(r'^veget_json$', 'map.views.veget_json', name='veget_json'),  
     url(r'^get-old-info$', 'map.views.get_old_info', name='get_old_info'),  
@@ -25,6 +27,9 @@ urlpatterns = patterns('',
     url(r'^ndvi001250-json/$', GetPolygonJsonNdvi001250.as_view(model=Ndvi001250,properties=('gid','gridcode'))),
     
     url(r'^vegetation-json/$', GetPolygonJsonVegetation.as_view(model=Vegetation,properties=('gid','structure','struct'))),
+    
+    url(r'^burning-json/$', GetPolygonJsonBurning.as_view(model=Burning,properties=('gid','gridcode'))),
+    
     url(r'^vegetstructure-json/$', GetPolygonJsonVeget.as_view(model=Veget,properties=('gid','structure','struct'))),
     url(r'^slope_json/$', 'map.views.slope_json', name='slope_json'),
     
--- map/views.py
+++ map/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import loader, RequestContext
-from map.models import Cotter, Radiation, Vegetation, Veget, Slope, Structure, Effectiveness, Ndvi001250
+from map.models import Cotter, Radiation, Vegetation, Veget, Slope, Structure, Effectiveness, Ndvi001250, Burning
 from djgeojson.views import GeoJSONLayerView
 from djgeojson.serializers import Serializer as GeoJSONSerializer
 # Create your views here.
@@ -30,6 +30,12 @@ def vegetation(request):
     context = {}
     return render_to_response('vegetation.html', context, RequestContext(request))
     
+
+def burning(request):
+    context = {}
+    return render_to_response('burning.html', context, RequestContext(request))
+
+
 
 def veget(request):
     context = {}
@@ -63,6 +69,11 @@ def ndvi001250_json(request):
 def vegetation_json(request):
     context = {'structures': Structure.objects.all()}
     return render_to_response('vegetation-json.html', context, RequestContext(request))
+
+
+def burning_json(request):
+    context = {'structures': Structure.objects.all()}
+    return render_to_response('burning-json.html', context, RequestContext(request))
 
 
 def veget_json(request):    
@@ -206,6 +217,45 @@ class GetPolygonJsonVegetation(GeoJSONLayerView):
         from fbr.settings import BASE_DIR
         import os.path
         cpath = BASE_DIR+'/map_cache/vegetation.txt'
+        if(os.path.exists(cpath)):
+            from django.http import HttpResponse
+            f = open(cpath,'r')
+            out = f.read()
+            f.close()
+            return HttpResponse(out, content_type="application/json")
+
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        serializer = GeoJSONSerializer()
+        response = self.response_class(**response_kwargs)
+        options = dict(#properties=self.properties,
+                       #precision=self.precision,
+                       simplify=self.simplify,
+                       srid=self.srid,
+                       geometry_field=self.geometry_field,
+                       force2d=self.force2d)
+        serializer.serialize(self.get_queryset(), stream=response, ensure_ascii=False,
+                             **options)
+
+        #import pdb; pdb.set_trace()
+        f = open(cpath,'w')
+        f.write(response.content)
+        f.close()
+        return response
+
+
+
+class GetPolygonJsonBurning(GeoJSONLayerView):
+    from fbr.settings import BASE_DIR
+    #precision = 4   # float
+    #simplify = 0.5  # generalization
+    def get_queryset(self):
+        return Burning.objects.all()
+    def render_to_response(self, context, **response_kwargs):
+        from fbr.settings import BASE_DIR
+        import os.path
+        cpath = BASE_DIR+'/map_cache/burning.txt'
         if(os.path.exists(cpath)):
             from django.http import HttpResponse
             f = open(cpath,'r')
