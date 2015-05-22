5aced5dac56dad8e83fdfb8f574aca60b9d8e36c
zdimon <zdimon77@gmail.com>
2015-05-22 03:52:08 +0300

    auto

--- map/management/commands/count_ndvi_category.py
+++ map/management/commands/count_ndvi_category.py
@@ -25,14 +25,19 @@ class Command(BaseCommand):
 
             if min_ef['gridcode__min'] <= s.gridcode < (min_ef['gridcode__min'] + h):
                 s.ndvi_category = 1
+                s.fuel_load = 5
             elif (min_ef['gridcode__min'] + h) <= s.gridcode < (min_ef['gridcode__min'] + 2*h):
                 s.ndvi_category = 2
+                s.fuel_load = 10
             elif (min_ef['gridcode__min'] + Decimal(2)*h) <= s.gridcode < (min_ef['gridcode__min'] + 3*h):
                 s.ndvi_category = 3
+                s.fuel_load = 15
             elif (min_ef['gridcode__min'] + Decimal(3)*h) <= s.gridcode < (min_ef['gridcode__min'] + 4*h):
-                s.ndvi_category = 4 
+                s.ndvi_category = 4
+                s.fuel_load = 20 
             elif (min_ef['gridcode__min'] + Decimal(4)*h) <= s.gridcode <= max_ef['gridcode__max']:
-                s.ndvi_category = 5       
+                s.ndvi_category = 5
+                s.fuel_load = 30       
         
                 
                 
